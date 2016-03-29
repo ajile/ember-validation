c2ff97fef19890c623d85198695f5a720c1f958e
ajile <v.v.milkov@gmail.com>
2016-03-29 16:24:21 +0300

    Little commit

--- tests/dummy/app/router.js
+++ tests/dummy/app/router.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 import config from './config/environment';
 
+var Prism = Prism || {};
+
 var Router = Ember.Router.extend({
   location: config.locationType,
   didTransition() {
