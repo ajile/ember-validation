f0f73782d8ca21d05cb09c833cd4af20fa9e7016
Dmitry Levashov <dio@std42.ru>
2016-04-06 11:57:06 +0300

    jsdocs comments updated

--- addon/mixins/element-mediator.js
+++ addon/mixins/element-mediator.js
@@ -4,17 +4,28 @@ export default Ember.Mixin.create({
   /** @type {Ember/Component} */
   view : null,
 
+  /**
+  * Attach event listeners to view
+  *
+  * @function
+  * @returns {undefined}
+  */
   viewDidChange: Ember.on('init', Ember.observer('view', function () {
     let view = this.get('view');
 
     if (view) {
       view
         .on('focusIn', this, this._onFocusIn)
-        .on('focusOut', this, this._onFocusOut)
-
+        .on('focusOut', this, this._onFocusOut);
     }
   })),
 
+  /**
+  * Remove event listeners from view befor destroy
+  *
+  * @function
+  * @returns {undefined}
+  */
   willDestroy() {
     var view = this.get('view');
 
@@ -27,10 +38,24 @@ export default Ember.Mixin.create({
     }
   },
 
+  /**
+  * Trigger event after view focused in
+  *
+  * @function
+  * @fires hideErrors
+  * @returns {undefined}
+  */
   _onFocusIn() {
     this.trigger('hideErrors', this.get('attribute'));
   },
 
+  /**
+  * Validate and trigger event after view focused out
+  *
+  * @function
+  * @fires showErrors
+  * @returns {undefined}
+  */
   _onFocusOut() {
     this.trigger('showErrors', this.get('attribute'));
     this.validate();
