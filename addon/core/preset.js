aa0b8a6446a8fdf082940755274cb69ac914e884
ajile <v.v.milkov@gmail.com>
2016-05-03 16:16:44 +0300

    Base class of the preset

---
+++ addon/core/preset.js
@@ -0,0 +1,44 @@
+import Ember from "ember";
+
+const { get } = Ember;
+
+/**
+ * @module
+ * @augments Ember.Object
+ * @public
+ */
+export default Ember.Object.extend({
+
+  /**
+   * @type Object
+   * @default {}
+   * @public
+   */
+  options: {},
+
+  /**
+   * @function
+   * @return {Object} scheme validation
+   * @public
+   */
+  evolve() {
+    const options = this.get("options"),
+          scheme = { options, validators: [] };
+
+    get(options, "required") && scheme.validators.push({ "name": "required" });
+
+    return this._evolve(scheme, options);
+  },
+
+  /**
+   * @function
+   * @param {Object} scheme
+   * @param {Object} options
+   * @return {Object}
+   * @private
+   */
+  _evolve(scheme, options) {
+    return scheme;
+  }
+
+});
