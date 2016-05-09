4b9c14be2857a0871c88e8b12f3d722fa1753fc3
ajile <v.v.milkov@gmail.com>
2016-05-09 13:12:17 +0300

    Added email preset

---
+++ addon/presets/email.js
@@ -0,0 +1,23 @@
+import Ember from "ember";
+import BasePreset from 'ember-validation/core/preset';
+
+/**
+ * @module
+ * @augments module:addon/core/preset
+ * @public
+ */
+export default BasePreset.extend({
+
+  /**
+   * @function
+   * @param {Object} scheme
+   * @param {Object} options
+   * @return {Object}
+   * @private
+   */
+  _evolve(scheme, options) {
+    scheme.validators.push({ "name": "email", options });
+    return scheme;
+  }
+
+});
