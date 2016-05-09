55067176f7499d26849171409722aa9f0507050f
ajile <v.v.milkov@gmail.com>
2016-05-09 13:13:45 +0300

    Email preset implemented to company model

--- tests/dummy/app/models/company.js
+++ tests/dummy/app/models/company.js
@@ -4,6 +4,7 @@ import ValidationMixin from "ember-validation/mixins/model";
 const { attr } = DS;
 
 export default DS.Model.extend(ValidationMixin, {
-  name: attr("string", { required: true }),
-  age: attr("number", { max: 5 })
+  name: attr("string", { required: true, isValidatable: true }),
+  email: attr("string", { preset: "email", required: true, isValidatable: true }),
+  age: attr("number", { max: 5, isValidatable: true })
 });
