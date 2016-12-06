5b94f9e48150148d4a72080d011c5de35f96aca8
ajile <v.v.milkov@gmail.com>
2016-12-06 15:21:03 +0300

    Added extra field to the validation error

--- addon/core/error.js
+++ addon/core/error.js
@@ -14,27 +14,55 @@ export default Ember.Object.extend({
    * A key of the error. It can be used for translation needs.
    * @type String
    * @public
-  */
+   */
   key: "",
 
   /**
    * A value of the attribute that is not valid.
    * @type String
    * @public
-  */
+   */
   value: "",
 
   /**
    * Options the validator was invoked with.
    * @type Object
    * @public
-  */
-  options: {},
+   */
+  options: { },
+
+  /**
+   * An error's extra data. In common case contains preprocessed data for using
+   * in locale files. The most popular addon for translation doesn't support
+   * Handlebar helpers in locale dicts.
+   *
+   * Property might be set by a validator or mediator for purpose of showing in
+   * template.
+   *
+   * For instance validator `date` when `options.min` is sent, checks that
+   * date-to-check is greater then date in `options.min`. If it's not -
+   * validator raises an error with a translation key. Then we show a human-error in
+   * accordance with that key.
+
+   * But how to notice user about min date? We could place error phrase like
+   * "You should set date greater then {{options.min}}", but in this case user
+   * will see something like that: "You should set date greater then Tue Dec 06
+   * 2016 15:09:43 GMT+0300 (MSK)". It's not readable. But we can not to use
+   * helpers (like `format-date`) inside the translation phrases - it doesn't
+   * work.
+   *
+   * For this purpose this property was created. Validators will pass extra data
+   * to the error, that we can show to user.
+   *
+   * @type Object
+   * @public
+   */
+  extra: { },
 
   /**
     @method toString
     @return {String}
-  */
+   */
   toString: function() {
     const { key, value } = getProperties(this, "key", "value");
     return `<ValidationError("${key}" for "${value}")>`;
--- addon/utils/error.js
+++ addon/utils/error.js
@@ -1,5 +1,5 @@
 import ErrorClass from 'ember-validation/core/error';
 
-export function createError(key, value, validatorName) {
-  return ErrorClass.create({ key, value, validatorName });
+export function createError(key, value, validatorName, extra={}) {
+  return ErrorClass.create({ key, value, validatorName, extra });
 }
