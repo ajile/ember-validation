30a5b1d75a195ac8ca3ae7305b4a8602744dd923
ajile <v.v.milkov@gmail.com>
2018-12-07 14:31:51 +0300

    Изменения сценария Travis

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,24 +3,6 @@ module.exports = {
     {
       name: 'default',
       dependencies: { }
-    },
-    {
-      name: 'wheely-projects-1',
-      dependencies: {
-        'ember': 'components/ember#1.13.13'
-      },
-      resolutions: {
-        'ember': '1.13.13'
-      }
-    },
-    {
-      name: 'wheely-projects-2',
-      dependencies: {
-        'ember': 'components/ember#2.0.0'
-      },
-      resolutions: {
-        'ember': '2.0.0'
-      }
     }
   ]
 };
